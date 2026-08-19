6f88be8fc8d089e208eb2dd23677bb9ef7b42670
Kauan-Teixeira-Poma <kauanpoma7@gmail.com>
2026-08-19 00:39:07 +0000

    homi

---
+++ ecomerci/src/test/java/com/example/ecomerci/EcomerciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecomerci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
